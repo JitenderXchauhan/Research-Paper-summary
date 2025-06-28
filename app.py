6c5bf2d18f4b936382140b3539b459a57f9539b5
jitender chauhan <139338140+JitenderXchauhan@users.noreply.github.com>
2025-06-28 18:52:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import gradio as gr
 from model import extract_text_from_pdf, chunk_text, summarize_chunks, save_summary_to_pdf
+import os 
 
 def process_pdf(pdf_file):
     try:
@@ -15,7 +16,7 @@ def process_pdf(pdf_file):
 
 demo = gr.Interface(
     fn=process_pdf,
-    inputs=gr.File(label="Upload PDF", type="filepath"), 
+    inputs=gr.File(label="Upload PDF", type="filepath"),
     outputs=[
         gr.Textbox(label="Generated Summary", lines=20),
         gr.File(label="Download PDF Summary")
@@ -24,6 +25,6 @@ demo = gr.Interface(
     description="Upload a research paper PDF. This app extracts, summarizes, and gives you a summarized PDF using Groq's LLaMA model."
 )
 
-
 if __name__ == "__main__":
-    demo.launch(share=True)
+    port = int(os.environ.get("PORT", 7860)) 
+    demo.launch(server_name="0.0.0.0", server_port=port)
